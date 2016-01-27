e75a835a7de52d5bb5288e494c88032ea8c1b752
ZhaoJunfeng <981248356@qq.com>
2016-01-27 20:57:47 +0800

    添加账户地址模板，后台订单管理类

---
+++ templates/my-account.php
@@ -0,0 +1,75 @@
+<?php get_header();?>
+    <div class="primary-goods-bags ud-container mt-20">
+            <div class="col-sm-2 primary-user-center">
+                <ul>
+                    <li><a href="#"><i class="iconfont">&#xe605;</i>全部订单</a></li>
+                    <li><a href="#"><i class="iconfont">&#xe60c;</i>待发货</a></li>
+                    <li><a href="#"><i class="iconfont">&#xe604;</i>已发货</a></li>
+                    <li><a href="#"><i class="iconfont">&#xe608;</i>待付款</a></li>
+                    <li><a href="#"><i class="iconfont">&#xe60e;</i>待收货</a></li>
+                    <li><a href="#"><i class="iconfont">&#xe609;</i>已完成</a></li>
+                </ul>
+                <ul class="mt-40">
+                    <li><a href="#"><i class="iconfont">&#xe603;</i>收获地址</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-10 mt-10">
+                <h4 class="border-1 pb-15">全部订单</h4>
+                <div class="primary-user-center-list f14">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>订单信息</th>
+                                <th>单价</th>
+                                <th>数量</th>
+                                <th>订单总额</th>
+                                <th>订单状态</th>
+                                <th>操作</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="tc">
+                                <td class="tl">
+                                    <img class="fl" src="images/small_4kc.png" />
+                                    <div class="fl mt-10">
+                                        <p class="f16">大疆无人机4K智能飞行器</p>
+                                        <p class="color-8"><span class="mr-10">白色</span><span>标准版</span></p>
+                                    </div>
+                                </td>
+                                <td>¥4999</td>
+                                <td>1</td>
+                                <td>¥4999</td>
+                                <td>待发货</td>
+                                <td class="cp"><i class="iconfont f20">&#xe602;</i></td>
+                                
+                            </tr>
+                            <tr class="tc">
+                                <td class="tl">
+                                    <img class="fl" src="images/small_4kc.png" />
+                                    <div class="fl mt-10">
+                                        <p class="f16">大疆无人机4K智能飞行器</p>
+                                        <p class="color-8"><span class="mr-10">白色</span><span>标准版</span></p>
+                                    </div>
+                                </td>
+                                <td>¥4999</td>
+                                <td>1</td>
+                                <td>¥4999</td>
+                                <td>待发货</td>
+                                <td class="cp"><i class="iconfont f20">&#xe602;</i></td>
+                                
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div class="priary-pagenav tc">
+                        <ul class="disinblock">
+                            <li><a href="#">1</a></li>
+                            <li><a href="#">2</a></li>
+                            <li><a href="#">3</a></li>
+                            <li><a href="#">4</a></li>
+                            <li><a href="#">></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+    </div>
+<?php get_footer();?>
---
+++ templates/my-address.php
@@ -0,0 +1,82 @@
+<?php get_header();?>
+
+    <div class="primary-goods-bags ud-container mt-20">
+    		<div class="col-sm-2 primary-user-center">
+    			<ul>
+    				<li><a href="#"><i class="iconfont">&#xe605;</i>全部订单</a></li>
+    				<li><a href="#"><i class="iconfont">&#xe60c;</i>待发货</a></li>
+    				<li><a href="#"><i class="iconfont">&#xe604;</i>已发货</a></li>
+    				<li><a href="#"><i class="iconfont">&#xe608;</i>待付款</a></li>
+    				<li><a href="#"><i class="iconfont">&#xe60e;</i>待收货</a></li>
+    				<li><a href="#"><i class="iconfont">&#xe609;</i>已完成</a></li>
+    			</ul>
+    			<ul class="mt-40">
+    				<li><a href="#"><i class="iconfont">&#xe603;</i>收获地址</a></li>
+    			</ul>
+    		</div>
+    		<div class="col-sm-10 mt-10">
+    			<h4 class="border-1 pb-15">收获地址</h4>
+    			<div class="primary-user-center-list f14 mt-40">
+				<ul>
+					<li class="col-sm-12  mb-20">
+						<div class="col-sm-1">李雷</div>
+						<div class="col-sm-2">13999999999</div>
+						<div class="col-sm-6">山东省济南市高新区</div>
+						<div class="col-sm-3 "><i class="iconfont fr f20 cp primary-address-del-tips">&#xe613;</i><i class="iconfont fr f20 cp mr-10 primary-address-set-tips">&#xe60f;</i><i class="iconfont fr f20 mr-10 cp primary-address-edit-tips" onclick="primary_address_edit()">&#xe611;</i></div>
+						<div class="cb"></div>
+					</li>
+					<li class="col-sm-12 mb-20">
+						<div class="col-sm-1">李雷</div>
+						<div class="col-sm-2">13999999999</div>
+						<div class="col-sm-6">山东省济南市高新区</div>
+						<div class="col-sm-3"><i class="iconfont fr f20 cp primary-address-del-tips">&#xe613;</i><i class="iconfont fr f20 cp mr-10 primary-address-set-tips">&#xe60f;</i><i class="iconfont fr f20 mr-10 cp primary-address-edit-tips" onclick="primary_address_edit()">&#xe611;</i></div>
+						<div class="cb"></div>
+					</li>
+					<div class="cb"></div>
+				</ul>
+				<div class="disinblock p10 border-green radius5 cp" id="primary-address-new">新增收获地址</div>
+    			</div>
+    			<!--收获地址新建修改-->
+    			<div class="primary-address-new pb-20">
+    				<form id="address-edit-validate" method="post" action="" class="form-horizontal form" novalidate="novalidate">
+			    		<div class="primary-login-title color-white f20">修改收获地址</div>
+			    		<div class="p10">
+						<label class="form-label tr col-sm-4 col-sm-3 mt-20">收获地区：</label>
+						<div class="formControls col-sm-8 col-sm-9">
+							<div id="primary-city-secect" class="mt-10">
+  								<select class="prov"></select> 
+    								<select class="city" disabled="disabled"></select>
+        							<select class="dist" disabled="disabled"></select>
+    							</div>
+						</div>
+					</div>
+					<div class="p10">
+						<label class="form-label tr col-sm-4 col-sm-3 mt-20">详细地址：</label>
+						<div class="formControls col-sm-8 col-sm-9">
+							
+							<textarea class="textarea radius5 mb-5 p10 mt-10" name="address_detailed"></textarea>
+						</div>
+					</div>
+					<div class="p10">
+						<label class="form-label tr col-sm-4 col-sm-3 mt-20">收件人：</label>
+						<div class="formControls col-sm-8 col-sm-9">
+							<input type="text" name="resginer_name" class="mb-5 input-text radius5 p10 mt-10 required" autocomplete="off" placeholder="请输入收件人姓名">
+						</div>
+					</div>
+					<div class="p10">
+						<label class="form-label tr col-sm-4 col-sm-3 mt-20">联系电话：</label>
+						<div class="formControls col-sm-8 col-sm-9">
+							<input type="text" name="address_tel" class="mb-5 input-text radius5 p10 mt-10 required" autocomplete="off" placeholder="请输入收件人联系电话">
+						</div>
+					</div>
+					<div class="p10">
+						<label class="form-label col-sm-4 col-sm-3 "></label>
+						<div class="formControls col-sm-8 col-sm-9 mt-20">
+							<input class="btn btn-default radius5" type="submit" value="修改">
+						</div>
+					</div>
+		  </form>
+    			</div>
+    		</div>
+    </div>
+<?php get_footer();?>
