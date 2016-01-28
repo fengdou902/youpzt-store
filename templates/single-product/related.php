dbf37b4bbef38e3c1ef1d43be58751f73cebc052
xsstc1 <xsstc1@163.com>
2016-01-28 18:20:11 +0800

    添加链接

--- templates/single-product/related.php
+++ templates/single-product/related.php
@@ -15,32 +15,47 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<div class="primary-products-information-about p10 mb-20">
 			<h3 class="mt-40">相关产品</h3>
 			<ul>
-				<li class="col-sm-2">
+				<li class="col-sm-2 p5">
 					<img src="<?php echo UPSTORE_PLUGIN_ASSETS_URI;?>images/small_4kc.png" />
-					<p class="bg-color-hui1">
+					<div class="bg-color-hui1 p10">
 					<div class="f14">大疆无人机</div>
-					<div><span>¥4999</span><span class="fr"><i class="iconfont color-blue ml-5">&#xe600;</i></span></div>
-					</p>
+					<div><span>¥4999</span><span class="fr cp add_to_cart" data-productid="<?php the_ID();?>"><i title="加入购物车" class="iconfont color-blue ml-5">&#xe600;</i></span></div>
+					</div>
 				</li>
-				<li class="col-sm-2">
+				<li class="col-sm-2 p5">
 					<img src="<?php echo UPSTORE_PLUGIN_ASSETS_URI;?>images/small_4kc.png" />
-					<p>大疆无人机</p>
+					<div class="bg-color-hui1 p10">
+					<div class="f14">大疆无人机</div>
+					<div><span>¥4999</span><span class="fr cp add_to_cart" data-productid="<?php the_ID();?>"><i title="加入购物车" class="iconfont color-blue ml-5">&#xe600;</i></span></div>
+					</div>
 				</li>
-				<li class="col-sm-2">
+				<li class="col-sm-2 p5">
 					<img src="<?php echo UPSTORE_PLUGIN_ASSETS_URI;?>images/small_4kc.png" />
-					<p>大疆无人机</p>
+					<div class="bg-color-hui1 p10">
+					<div class="f14">大疆无人机</div>
+					<div><span>¥4999</span><span class="fr cp add_to_cart" data-productid="<?php the_ID();?>"><i title="加入购物车" class="iconfont color-blue ml-5">&#xe600;</i></span></div>
+					</div>
 				</li>
-				<li class="col-sm-2">
+				<li class="col-sm-2 p5">
 					<img src="<?php echo UPSTORE_PLUGIN_ASSETS_URI;?>images/small_4kc.png" />
-					<p>大疆无人机</p>
+					<div class="bg-color-hui1 p10">
+					<div class="f14">大疆无人机</div>
+					<div><span>¥4999</span><span class="fr cp add_to_cart" data-productid="<?php the_ID();?>"><i title="加入购物车" class="iconfont color-blue ml-5">&#xe600;</i></span></div>
+					</div>
 				</li>
-				<li class="col-sm-2">
+				<li class="col-sm-2 p5">
 					<img src="<?php echo UPSTORE_PLUGIN_ASSETS_URI;?>images/small_4kc.png" />
-					<p>大疆无人机</p>
+					<div class="bg-color-hui1 p10">
+					<div class="f14">大疆无人机</div>
+					<div><span>¥4999</span><span class="fr cp add_to_cart" data-productid="<?php the_ID();?>"><i title="加入购物车" class="iconfont color-blue ml-5">&#xe600;</i></span></div>
+					</div>
 				</li>
-				<li class="col-sm-2">
+				<li class="col-sm-2 p5">
 					<img src="<?php echo UPSTORE_PLUGIN_ASSETS_URI;?>images/small_4kc.png" />
-					<p>大疆无人机</p>
+					<div class="bg-color-hui1 p10">
+					<div class="f14">大疆无人机</div>
+					<div><span>¥4999</span><span class="fr cp add_to_cart" data-productid="<?php the_ID();?>"><i title="加入购物车" class="iconfont color-blue ml-5">&#xe600;</i></span></div>
+					</div>
 				</li>
 				<div class="cb"></div>
 			</ul>
